7f2c408d12e9269a3ee0988020dd652e5db24d45
Shitovoy <boyenerji2014@icloud.com>
2024-10-08 20:09:29 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import './assets/main.css'
+import './assets/tailwind.css'
+import Aura from '@primevue/themes/aura';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';  
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.mount('#app')
+
+
